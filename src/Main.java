44bce46b7fb1d484cdaf8a791cd16007c5a61e5b
Christoph Dalpiaz <christoph.dalpiaz@gmail.com>
2019-01-27 13:43:17 +0100

    Minor improvements

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        start();
+    }
 
+    private static void start()
+    {
         MainController mainController = new MainController();
-
         mainController.getWord();
         mainController.createListValidWords();
         mainController.tryWords();
         mainController.removeInvalidWords();
         mainController.printValidWords();
+
+        start();
     }
 }
